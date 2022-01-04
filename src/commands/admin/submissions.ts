055999d0a4ee9aadeb0fe4f9c6451cafec269d17
TymanWasTaken <tyman@tyman.tech>
2022-01-04 15:27:40 +0000

    feat(Admin): Submissions command

---
+++ src/commands/admin/submissions.ts
@@ -0,0 +1,43 @@
+import { BotCommand } from '@lib/ext/BotCommand';
+import { App, Submission } from '@lib/models';
+import { Message } from 'discord.js';
+
+export default class SubmissionsCommand extends BotCommand {
+	constructor() {
+		super('submissions', {
+			aliases: ['submissions'],
+			description: {
+				content: () => this.client.i18n.t('COMMANDS.REVIEW_DESCRIPTION'),
+				usage: 'submissions',
+				examples: ['submissions']
+			},
+			channel: 'guild',
+			permissionCheck: 'admin'
+		});
+	}
+
+	public async exec(message: Message) {
+		const submissions = await Submission.findAll({
+			where: {
+				guild: message.guild!.id
+			}
+		});
+		if (submissions.length < 1) {
+			await message.util?.send(this.client.i18n.t('COMMANDS.NO_SUBMISSIONS'));
+		}
+		let string = '';
+		for (const submission of submissions) {
+			const app = await App.findByPk(submission.position);
+			const user = await this.client.users.fetch(submission.author);
+			string += `<@${submission.author}> (${user.tag}): ${app!.name}\n`;
+		}
+		await message.util!.send({
+			embeds: [
+				this.client.util
+					.embed()
+					.setTitle('Submitted applications')
+					.setDescription(string)
+			]
+		});
+	}
+}
