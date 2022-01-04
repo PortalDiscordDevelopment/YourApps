9ce6b7a6c099c1a8207ca0f266bd4162e5f31fed
TymanWasTaken <tyman@tyman.tech>
2022-01-04 15:40:28 +0000

    feat(Dev): Update command to quickly update the bot

---
+++ src/commands/dev/update.ts
@@ -0,0 +1,34 @@
+import { Message } from 'discord.js';
+import { BotCommand } from '@lib/ext/BotCommand';
+
+export default class UpdateCommand extends BotCommand {
+	constructor() {
+		super('update', {
+			aliases: ['update'],
+			description: {
+				content: () => this.client.i18n.t('COMMANDS.DESCRIPTIONS.UPDATE'),
+				usage: 'update',
+				examples: ['update']
+			},
+			ownerOnly: true,
+			typing: true
+		});
+	}
+
+	public async exec(message: Message) {
+		try {
+			await message.util!.send('Git pulling...')
+			await this.client.util.shell(`git pull`);
+			await message.util!.send('Testing build...')
+			await this.client.util.shell(`yarn build`);
+			await message.util!.send('Restarting bot...')
+			await this.client.util.shell(`pm2 restart yourapps`);
+		} catch (e) {
+			return message.util!.send(
+				this.client.i18n.t('DEVELOPER.ERROR_RELOADING', {
+					link: await this.client.util.haste(e.stack)
+				})
+			);
+		}
+	}
+}
