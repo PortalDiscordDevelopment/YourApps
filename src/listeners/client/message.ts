23461c820fac66452729fb79c9d3aebc77cc682a
TymanWasTaken <tyman@tyman.tech>
2022-01-17 16:16:47 -0700

    fix: Make min join time work and also add checks before starting application with a custom command (#3)

--- src/listeners/client/message.ts
+++ src/listeners/client/message.ts
@@ -19,7 +19,33 @@ export default class MessageListener extends BotListener {
 		});
 		for (const app of apps) {
 			if (!app.customcommand || message.content != app.customcommand) continue;
+			if (
+				(await message.author.send({}).catch(e => e.message)) ==
+				'Cannot send messages to this user'
+			) {
+				await message.util!.send(this.client.i18n.t('ERRORS.CANNOT_DM'));
+				return;
+			}
+			const memberRoles = (await message.member!.fetch()).roles.cache;
+			if (!app.requiredroles.every(r => memberRoles.has(r))) {
+				await message.util!.send(
+					this.client.i18n.t('ERRORS.NO_REQUIRED_ROLES')
+				);
+				return;
+			}
+			if (
+				app.minjointime &&
+				(message.editedTimestamp ?? message.createdTimestamp) -
+					message.member!.joinedTimestamp! <
+					app.minjointime
+			) {
+				await message.util!.send(
+					this.client.i18n.t('ERRORS.NOT_JOINED_LONG_ENOUGH')
+				);
+				return;
+			}
 			await ApplyCommand.startApplication(message, app);
+			break;
 		}
 	}
 }
