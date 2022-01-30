b9ef2104871509cd72c9ef63422aa1aa41013a4c
--global <tyman@tyman.tech>
2022-01-30 18:19:27 +0000

    fix: Improve admin and review permission checking

--- src/inhibitors/permissions/adminCheck.ts
+++ src/inhibitors/permissions/adminCheck.ts
@@ -13,16 +13,26 @@ export default class AdminCheckInhibitor extends BotInhibitor {
 		if (command.permissionCheck != 'admin') return false;
 		if (message.guild == null) return false;
 		const guildEntry = await Guild.findByPk(message.guild.id);
+		const member = await message.member!.fetch();
+		let canUseCommand = false;
 		if (
-			// If they do not have an admin role, but with all the bases covered
-			!guildEntry ||
-			guildEntry.adminroles.length < 1 ||
-			!message.member!.roles.cache.some(r =>
-				guildEntry.adminroles!.includes(r.id)
-			)
+			guildEntry &&
+			guildEntry.adminroles.length >= 1 &&
+			member.roles.cache.some(r => guildEntry.adminroles.includes(r.id))
 		) {
-			if (message.member!.permissions.has('MANAGE_GUILD')) return false;
-			else return true;
-		} else return false;
+			// Admin roles exist and user has one
+			canUseCommand = true;
+		} else if (
+			guildEntry &&
+			guildEntry.adminroles.length < 1 &&
+			member.permissions.has('MANAGE_GUILD')
+		) {
+			// Admin roles do not exist and user has MANAGE_GUILD
+			canUseCommand = true;
+		} else if (member.permissions.has('ADMINISTRATOR')) {
+			// User has admin permission, override everything
+			canUseCommand = true;
+		}
+		return !canUseCommand;
 	}
 }
--- src/inhibitors/permissions/reviewerCheck.ts
+++ src/inhibitors/permissions/reviewerCheck.ts
@@ -14,21 +14,39 @@ export default class ReviewerCheckInhibitor extends BotInhibitor {
 		if (message.guild == null) return false;
 		const guildEntry = await Guild.findByPk(message.guild.id);
 		const member = await message.member!.fetch();
+		let canUseCommand = false;
 		if (
-			// If they do not have a review role, but with all the bases covered
 			guildEntry &&
 			guildEntry.reviewroles.length >= 1 &&
 			member.roles.cache.some(r => guildEntry.reviewroles.includes(r.id))
 		) {
-			return false;
+			// Review roles exist and user has one
+			canUseCommand = true;
 		} else if (
 			guildEntry &&
 			guildEntry.reviewroles.length < 1 &&
 			member.permissions.has('MANAGE_ROLES')
 		) {
-			return false;
-		} else {
-			return true;
+			// Review roles do not exist and user has MANAGE_ROLES
+			canUseCommand = true;
+		} else if (
+			guildEntry &&
+			guildEntry.adminroles.length >= 1 &&
+			member.roles.cache.some(r => guildEntry.adminroles.includes(r.id))
+		) {
+			// Admin roles exist and user has one
+			canUseCommand = true;
+		} else if (
+			guildEntry &&
+			guildEntry.adminroles.length < 1 &&
+			member.permissions.has('MANAGE_GUILD')
+		) {
+			// Admin roles do not exist and user has MANAGE_GUILD
+			canUseCommand = true;
+		} else if (member.permissions.has('ADMINISTRATOR')) {
+			// User has admin permission, override everything
+			canUseCommand = true;
 		}
+		return !canUseCommand;
 	}
 }
