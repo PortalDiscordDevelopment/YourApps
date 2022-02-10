e58c86fa1b8a9cec0acb654fc5bc051ab2b02951
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-02-09 21:22:31 -0700

    fix: Remove guild members intent, it is not needed

--- src/lib/extensions/BotClient.ts
+++ src/lib/extensions/BotClient.ts
@@ -75,7 +75,6 @@ export class BotClient extends AkairoClient {
 				},
 				intents: [
 					Intents.FLAGS.GUILDS,
-					Intents.FLAGS.GUILD_MEMBERS,
 					Intents.FLAGS.GUILD_MESSAGES,
 					Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
 					Intents.FLAGS.DIRECT_MESSAGES
