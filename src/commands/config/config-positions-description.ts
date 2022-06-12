fefd8ab5eeb731b2ea4188e0619987e34ace8293
TymanWasTaken <tbeckman530@gmail.com>
2022-06-12 19:24:39 -0400

    feat: Config positions description command

---
+++ src/commands/config/config-positions-description.ts
@@ -0,0 +1,80 @@
+import type { CommandOptions } from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
+import { BotCommand } from '../../lib/BotCommand';
+import type { AutocompleteInteraction, CommandInteraction } from 'discord.js';
+import { Op } from 'sequelize';
+import { Position } from '../../lib/models';
+
+@ApplyOptions<CommandOptions>({
+	name: 'config-positions-description',
+	description: 'Modifies the description of a position.',
+	preconditions: [],
+	slashOptions: {
+		options: [
+			{
+				name: 'position',
+				description: 'The position to modify the description of',
+				type: 'STRING',
+				autocomplete: true,
+				required: true
+			},
+            {
+                name: 'description',
+                description: 'The new description of the position',
+                type: 'STRING',
+                required: true
+            }
+		]
+	}
+})
+export class ConfigPositionsDescriptionCommand extends BotCommand {
+	override async chatInputRun(interaction: CommandInteraction) {
+		await interaction.deferReply();
+		const {
+			position: positionName,
+			description
+		}: {
+			position: string;
+			description: string;
+		} = this.parseArgs(interaction);
+		const position = await Position.findOne({
+			where: {
+				guild: interaction.guildId!,
+				name: positionName
+			}
+		});
+		if (!position) {
+			await interaction.editReply(
+				await this.t(interaction, {
+					context: 'position_not_found',
+					name: positionName
+				})
+			);
+			return;
+		}
+		position.description = description;
+		await position.save();
+		await interaction.editReply(
+			await this.t(interaction, {
+				name: positionName
+			})
+		);
+	}
+
+	// Autocomplete positions based on a substring search
+	override async autocompleteRun(interaction: AutocompleteInteraction) {
+		const positions = await Position.findAll({
+			where: {
+				name: {
+					[Op.substring]: interaction.options.getString('name', false) ?? ''
+				}
+			}
+		});
+		await interaction.respond(
+			positions.map(p => ({
+				name: `${p.id} - ${p.name}`,
+				value: p.name
+			}))
+		);
+	}
+}
--- src/options/config.example.ts
+++ src/options/config.example.ts
@@ -12,6 +12,7 @@ export const slashHints: Record<string, `${bigint}`> = {
 	'config-positions-delete': '985052733050585108',
 	'config-positions-roles-add': '985364185133219900',
 	'config-positions-roles-remove': '985368158808465428',
+	'config-positions-description': '985685194218537030',
 	ping: '985047539717922857',
 	info: '985047540879728661',
 	language: '985047541966053436',
