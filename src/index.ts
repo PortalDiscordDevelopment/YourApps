b37512e62cf0d719812d45f19a7fa45e2cbc235b
TymanWasTaken <tbeckman530@gmail.com>
2022-08-22 07:21:47 -0600

    Add better logger

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import { container, SapphireClient } from "@sapphire/framework";
 import * as config from "./config";
 import { ModuleStore } from "./structures/store";
 
+// Sapphire plugins
+import "@sapphire/plugin-logger/register";
+
 (async () => {
 	// Create the SapphireClient
-	const client = new SapphireClient({ intents: ["GUILDS"] });
+	const client = new SapphireClient({
+		intents: ["GUILDS"],
+		logger: {
+			level: config.logLevel
+		}
+	});
 
 	// Register and initialize the module store
 	container.stores.register(new ModuleStore());
